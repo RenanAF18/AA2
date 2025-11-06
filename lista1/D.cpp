005b4f2445aa3ed41504cd1798d411bdb6245be3
RenanAF18 <renansavio52@gmail.com>
2025-11-06 11:03:23 -0300

    lista1 D

--- lista1/D.cpp
+++ lista1/D.cpp
@@ -1,64 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 1 << 17 + 10;
+typedef long long ll;
+
+const int MAXN = (1 << 17) + 10;
 vector<ll> tree(4 * MAXN);
 vector<ll> a(MAXN);
 
+ll build(int idx, int l, int r, bool orr) {
+    if (l == r)
+        return tree[idx] = a[l];
 
-typedef long long ll;
-typedef pair<ll,ll> pii;
+    int mid = (l + r) / 2;
+    ll left = build(2 * idx, l, mid, !orr);
+    ll right = build(2 * idx + 1, mid + 1, r, !orr);
 
-ll build(int idx, int l, int r, bool orr){
-    if(l == r){
-        tree[idx] = a[l];
-        return tree[idx];
-    }
-    else{
-        int mid = (l + r)/2;
-        if(orr)
-            tree[idx] = build(2*idx,l, mid, false) | build(2*idx + 1, mid + 1, r, false);
-        else
-            tree[idx] = build(2*idx,l, mid, true) ^ build(2*idx + 1, mid + 1, r, true);
-        return tree[idx];
-    }
+    if (orr) tree[idx] = left | right;
+    else tree[idx] = left ^ right;
+
+    return tree[idx];
 }
 
-ll update(int idx, int l, int r, int pos, ll val, bool orr){
-    if(l == r)
+
+void update(int idx, int l, int r, int pos, ll val, bool orr) {
+    if (l == r) {
         tree[idx] = val;
-    else{
-        int mid = (l + r)/2;
-        
-        if(pos < mid)
-            update(idx*2, l, mid, pos, val, !orr);
-        else
-            update(idx*2 + 1, mid + 1, r, pos, val, !orr);
-    }     
+        return;
+    }
+    int mid = (l + r) / 2;
+    if (pos <= mid)
+        update(2 * idx, l, mid, pos, val, !orr);
+    else
+        update(2 * idx + 1, mid + 1, r, pos, val, !orr);
 
+    if (orr) tree[idx] = tree[2 * idx] | tree[2 * idx + 1];
+    else tree[idx] = tree[2 * idx] ^ tree[2 * idx + 1];
 }
 
-ll query(int idx, int l, int r, int lq, int rq){
-    if( l > rq || r < rq)
-        return 0;
-    
-    if(lq >= l && r <= rq)
-        return tree[idx];
-    
-    int mid = (l + r)/2;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-
-}
- 
-
-int main(){
-    cin.tie(0)->sync_with_stdio(0);
     int n, q;
-    cin >> n,q;
-    for(ll &x : a) cin >> x;
-    
+    cin >> n >> q;
 
-    
-    
-    return 0;
+    int size = 1 << n;
+    for (int i = 0; i < size; i++) cin >> a[i];
+
+    bool start_or = (n % 2 == 1);
+    build(1, 0, size - 1, start_or);
+
+    while (q--) {
+        int pos; ll val;
+        cin >> pos >> val;
+        
+        pos--;
+        update(1, 1-1, size - 1, pos, val, start_or);
+        cout << tree[1] << "\n";
+    }
 }
