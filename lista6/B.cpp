9310a1ae80a8e0146b8a175a2657a1815897317a
124211810 SAVIO RENAN ALVES FURTADO <savio.renan.alves.furtado@lcc2-53.lcc.ufcg.edu.br>
2025-11-28 11:08:05 -0300

    att lista6 B

--- lista6/B.cpp
+++ lista6/B.cpp
@@ -16,21 +16,49 @@ const int MAXK = 10;
 vector<pii> graph [MAXN];
 ll dist[MAXN][MAXK];
 
-void dijkstra(int start, int n){
-    priority_queue<tii, vector<tii>, greater<tii>> pq;
+void atualizar(int v, ll newDist, int k){
+    if(dist[v][k] >= newDist){
+        swap(dist[v][k], newDist);
+    }
+    for(int i = k; i > 0; i--){
+        if(dist[v][i] >= dist[v][i-1]){
+            swap(dist[v][i], dist[v][i-1]);
+        }
+
+    }
+}
+
+void dijkstra(int start, int n, int k){
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
 
     for(int i = 0; i <= n; i++){
         for(int j = 0; j < 10; j++)
             dist[i][j] = INF;
     }
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < 10; i++)
         dist[start][i] = 0;
-        pq.push({0, start, i});
-    }
+    
+    pq.push({0, start});
+
     
     while(!pq.empty()){
+        ll d, u;
+        d = pq.top().first;
+        u = pq.top().second;
+        pq.pop();
 
+        if(d > dist[u][k]) continue;
 
+        for(auto edge : graph[u]){
+            int v = edge.first;
+            ll w = edge.second;
+
+            for(int i = 0; i < k; i++){
+                ll newDist = w + dist[u][i];
+                atualizar(v, newDist, k);
+                pq.push({newDist, v});
+            }
+        }
     }
     
     
@@ -41,6 +69,15 @@ int main(){
     cin.tie(0)->sync_with_stdio(0);
     
     int n, m, k; cin >> n >> m >> k;
+    while(m--){
+        int a, b;
+        ll c;
+        cin >> a >> b >> c;
+        graph[a].push_back({b, c});
+    }
+
+    for(int i = 0; i < k; i++)
+        cout << dist[n][i] << " ";
     
     return 0;
 }
